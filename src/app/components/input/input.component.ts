d0cb2d49a65898fb993dffd2b07ff968a71cc24d
Caio Reis <caioreis@maxtrack.com.br>
2024-02-29 14:55:06 -0300

    feat

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,10 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component, EventEmitter, Input, NO_ERRORS_SCHEMA, OnInit, Output, SimpleChanges } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Body } from '@components/text/body/body.component';
+import { Heading } from '@components/text/heading/heading.component';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, Body, Heading],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
@@ -14,7 +16,12 @@ export class InputComponent {
   @Input() type: string | undefined;
   @Input() placeholder: string | undefined;
   @Input() icon: string | undefined;
+  @Input() errors!: string[];
+  @Input() hasError!: boolean;
+
+
   @Output() onChange = new EventEmitter<string>()
+
 
   value!: any;
 
